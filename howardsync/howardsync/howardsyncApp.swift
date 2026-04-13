35a2d6d3e494ade5307e849922937da3ff40eaa6
Aaryan1604 <aryanpanthi3@gmail.com>
2026-04-12 23:10:04 -0400

    Add initial Xcode project

---
+++ howardsync/howardsync/howardsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  howardsyncApp.swift
+//  howardsync
+//
+//  Created by Aaryan Panthi on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct howardsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
